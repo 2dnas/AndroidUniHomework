b63c0836da7640c2cd0f4768ed8c5e07a999e49a
Sandro <sandro.shanshiashvili.1@btu.edu.ge>
2021-03-28 11:23:26 +0400

    davaleba1

---
+++ src/Main.kt
@@ -0,0 +1,41 @@
+fun main() {
+    println(isComposite(1))
+    println(isComposite(2))
+    println(isComposite(3))
+    println(isComposite(5))
+    println(isComposite(7))
+    println(isComposite(11))
+    println(isComposite(13))
+    println(isComposite(17))
+    println(isComposite(41))
+    println(isComposite(43))
+
+
+}
+
+fun isComposite(number: Int): Boolean {
+    //neither composite or prime
+    if (number == 1) {
+        return false
+    }
+
+    if (number <= 3 || number == 5) {
+        return false
+    }
+
+    if (number % 2 == 0 || number % 3 == 0) {
+        return true
+    }
+
+    var i = 5
+    while (i * i <= number) {
+        if (number % i == 0 || number % (i + 2) == 0) {
+            return true
+        }
+        i += 6
+    }
+    return false;
+}
+
+
+
